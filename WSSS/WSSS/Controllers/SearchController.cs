3b2cf85dad4fc4c5b7ff2b2b344b375d234dca7c
Dmitri Soshnikov <dmitri@soshnikov.com>
2018-02-26 14:15:41 +0400

    First checkin

---
+++ WSSS/WSSS/Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WSSS.Controllers
+{
+    public class SearchController : ApiController
+    {
+        /*
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        */
+
+        // GET api/<controller>/<search term>
+        public string Get(string q)
+        {
+            return "Hello";
+        }
+        
+        /*
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+        */
+    }
+}
